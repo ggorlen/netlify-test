bbad37b914215673ea6f67224b20e177646581ba
ggorlen <gsgorlen@gmail.com>
2022-01-19 14:17:08 -0800

    try honeypot

--- pages/index.js
+++ pages/index.js
@@ -15,21 +15,26 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-        <form name="contact" method="POST" data-netlify="true">
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>   
+        <form
+          name="contact"
+          method="POST"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+        >
+          <p class="hidden">
+            <label>
+              Don’t fill this out if you’re human: <input name="bot-field" />
+            </label>
           </p>
           <p>
-            <label>Your Email: <input type="email" name="email" /></label>
+            <label>
+              Email: <input type="text" name="email" />
+            </label>
           </p>
           <p>
-            <label>Your Role: <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
           </p>
           <p>
             <button type="submit">Send</button>
